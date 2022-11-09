8d4588a56418e732cd5e7ab9ed347a21433cf791
elthef <elthef@hotmail.com>
2022-11-09 17:53:59 +0800

    Add files via upload

--- IAI_test20221109.py
+++ IAI_test20221109.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import time
 import threading
 from serial import Serial
@@ -18,8 +19,12 @@ MOVE_LOC5 = ":0106980000055C\r\n"
 CurrentPos = ":0103900000026A\r\n"
 FiftymmPos = ":0110990000020400001388B5\r\n"
 
+# constant parameters
 speed_combValues = ['2.5 mm/s', '4 mm/s', '5.2 mm/s','10 mm/s']
-
+max_distance = 200
+tgt_pos_band = '000A'  # Position band 0.01 mm pg 377
+tgt_pos_vel  = '2710'  # Velocity 0.01 mm/s
+tgt_pos_acc  = '001E'  # Acceleration 0.01 G
 
 # class InfiniteTimer():
 #     """A Timer class that does not stop, unless you want it to."""
@@ -173,16 +178,20 @@ def MoveCallBack(pos):
     global MF_tgt_pos
 
     MF_tgt_pos = int(pos)
-    dist = MF_tgt_pos * 100
-    # tgt_pos = 180 # in mm
-    # print(f"text entry: {pos}")
-    tgt_pos = str(f'{dist:0>4x}')
-    print('MoveCallBack',dist,len(tgt_pos),tgt_pos)
-    #IAI_MODBUSascii_20221104.move2Pos(tgt_pos)
-    #t1 = threading.Thread(target=IAI.move2Pos,args=(tgt_pos,))
-    t1 = threading.Thread(target=IAI.move2PoswifSpeed,args=(tgt_pos,))
-    # Start the threads
-    t1.start()
+    if MF_tgt_pos > 0 and MF_tgt_pos <= max_distance:
+        dist = MF_tgt_pos * 100
+        # tgt_pos = 180 # in mm
+        # print(f"text entry: {pos}")
+        tgt_pos = str(f'{dist:0>4x}')
+        #print('MoveCallBack',dist,len(tgt_pos),tgt_pos)
+        #IAI_MODBUSascii_20221104.move2Pos(tgt_pos)
+        #t1 = threading.Thread(target=IAI.move2Pos,args=(tgt_pos,))
+        t1 = threading.Thread(target=IAI.move2PoswifSpeed,args=(tgt_pos,tgt_pos_band, tgt_pos_vel, tgt_pos_acc,))
+        # Start the threads
+        t1.start()
+    else:
+        #raise ValueError(f'value {MF_tgt_pos} must be between 0 and 200')
+        messagebox.showerror("Error",f'The Position value entered {MF_tgt_pos} mm \n must be between 0 and 200' )
 
 def ResetCallBack(pos):
     IAI.ALRS()
@@ -198,7 +207,7 @@ class MainFrame(tk.Frame):
         # self.listbox = tk.Listbox(self,width=50, height=20, yscrollcommand=scrollbar.set)
         # self.listbox = tk.Listbox(self)
 
-        self.frame = customtkinter.CTkFrame(master=app, width=600, height=240, corner_radius=15)
+        self.frame = customtkinter.CTkFrame(master=app, width=600, height=300, corner_radius=15)
         self.frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
         self.frame.grid_columnconfigure(0, weight=1)
         self.frame.grid_columnconfigure(1, weight=1)
@@ -208,7 +217,7 @@ class MainFrame(tk.Frame):
         self.speedlabel.place(x=80.5, y=20.5, anchor=tk.CENTER)
         self.speedlabel.grid(row=2, column=0, columnspan=1, padx=10, pady=10, sticky="e")
 
-        combobox_speedvar = customtkinter.StringVar(value='5 mm/s')  # set initial value
+        self.combobox_speedvar = customtkinter.StringVar(value='5 mm/s')  # set initial value
         self.combobox_speed = customtkinter.CTkComboBox(master=self.frame, values=speed_combValues,
                                                         command=self.combobox_speed_selection)
         self.combobox_speed.grid(row=2, column=1, columnspan=1, pady=5, padx=5, sticky="e")
@@ -234,6 +243,10 @@ class MainFrame(tk.Frame):
                                                   command=lambda: MoveCallBack(int_var.get()))
         self.buttonReset.grid(row=7, column=0, columnspan=2, padx=20, pady=10, sticky="ew")
 
+        self.connectionStatuslabel = customtkinter.CTkLabel(master=self.frame, text="Not Connected", justify=tk.LEFT)
+        self.connectionStatuslabel.place(x=80.5, y=20.5, anchor=tk.CENTER)
+        self.connectionStatuslabel.grid(row=8, column=0, columnspan=1, padx=10, pady=10, sticky="e")
+
     def on_data(self, data):
         #print("Called from tk Thread:", data)
 
@@ -253,17 +266,36 @@ class MainFrame(tk.Frame):
             # print("error",len(data))
 
     def combobox_speed_selection(self, event):
+        global tgt_pos_vel
         # Three methods here all get the same value.
-        # print("change")
-        print(event)
+        #print("change")
+        #print(event)
+        #speed_combValues = ['2.5 mm/s', '4 mm/s', '5.2 mm/s', '10 mm/s']
+        match event:
+            case '2.5 mm/s':
+                print('2.5 mm/s')
+                temp = 2.5 * 100
+            case '4 mm/s':
+                temp = 4 * 100
+                print('4 mm/s')
+            case '5.2 mm/s':
+                temp = 5.2 * 100
+                print('5.2 mm/s')
+            case '10 mm/s':
+                temp = 10 * 100
+                print('10 mm/s')
+            case _:
+                temp = 0.01 * 100
+                print("default 0.01 mm/s")
 
+        tgt_pos_vel = str(f'{int(temp):0>4x}')
 
 if __name__ == '__main__':
     customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
     customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
     app = customtkinter.CTk(className='IAI Actuator Control')
     # set window size
-    app.geometry("400x300")
+    app.geometry("360x350")
     app.title("IAI control")
     #
     # app = tk.Tk(className='IAI Actuator Control')
@@ -276,8 +308,15 @@ if __name__ == '__main__':
 
     main_frame = MainFrame()
     # Set listener to our reader
-    serial_port = IAI.connect(portname="COM4", baudrate=38400)
-    print(serial_port)
+    while True:
+        serial_port = IAI.connect(portname="COM4", baudrate=38400)
+        if serial_port.isOpen():
+            main_frame.connectionStatuslabel.configure(text="Connected")
+            break
+        else:
+            main_frame.connectionStatuslabel.configure(text="Not Connected")
+            main_frame.connectionStatuslabel.showerror("Error",f'Please Check Seria port' )
+
     IAI.SerialReaderProtocolRaw.tk_listener = main_frame
     reader = ReaderThread(serial_port, IAI.SerialReaderProtocolRaw)
     reader.start()
